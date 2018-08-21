52f835bc4fa5f3ba15945f16d392a0c8b273d6e4
Christopher Dexter <Chris.Dexter@live.co.uk>
2018-08-21 16:00:58 +0100

    showing an error

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);    
+    this.state = {incorrectAnswer :false};
+  }
+
   render() {
     return (
       <main>
@@ -15,15 +20,17 @@ class QuizQuestion extends Component {
             })}
           </ul>
         </section>
+        { this.state.incorrectAnswer ? <p className='error'>That's not right</p>: null }
       </main>
     );
   }
 
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+      this.setState({incorrectAnswer :false});
       this.props.showNextQuestionHandler();
     } else {
-      alert('wrong answer');
+      this.setState({incorrectAnswer :true});
     }
   };
 }
